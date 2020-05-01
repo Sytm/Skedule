f191b653d38ed548280bc3e738259f2294e72cae
BrunoSilvaFreire <brunorbsf@gmail.com>
2020-05-01 14:07:06 -0400

    Added global syntax sugar functions

--- src/main/kotlin/com/okkero/skedule/BukkitDispatcher.kt
+++ src/main/kotlin/com/okkero/skedule/BukkitDispatcher.kt
@@ -7,7 +7,7 @@ import org.bukkit.plugin.java.JavaPlugin
 import org.bukkit.scheduler.BukkitTask
 import kotlin.coroutines.CoroutineContext
 
-private val bukkitScheduler
+internal val bukkitScheduler
     get() = Bukkit.getScheduler()
 
 
---
+++ src/main/kotlin/me/ddevil/skedule/Launchers.kt
@@ -0,0 +1,47 @@
+package me.ddevil.skedule
+
+import com.okkero.skedule.BukkitSchedulerController
+import com.okkero.skedule.SynchronizationContext
+import com.okkero.skedule.bukkitScheduler
+import com.okkero.skedule.schedule
+import org.bukkit.plugin.Plugin
+
+/**
+ * Sugar function to allow for easier creation of coroutines.
+ *
+ * For example, before:
+ * ```kotlin
+ * Bukkit.getScheduler().schedule(myPlugin) {
+ *     //...
+ * }
+ * ```
+ * After:
+ * ```kotlin
+ * skeduleSync(myPlugin) {
+ *     //...
+ * }
+ * ```
+ */
+fun skeduleSync(plugin: Plugin, block: suspend BukkitSchedulerController.() -> Unit) {
+    bukkitScheduler.schedule(plugin, SynchronizationContext.SYNC, block)
+}
+
+/**
+ * Sugar function to allow for easier creation of coroutines.
+ *
+ * For example, before:
+ * ```kotlin
+ * Bukkit.getScheduler().schedule(myPlugin, SynchronizationContext.ASYNC) {
+ *     //...
+ * }
+ * ```
+ * After:
+ * ```kotlin
+ * skeduleAsync(myPlugin) {
+ *     //...
+ * }
+ * ```
+ */
+fun skeduleAsync(plugin: Plugin, block: suspend BukkitSchedulerController.() -> Unit) {
+    bukkitScheduler.schedule(plugin, SynchronizationContext.ASYNC, block)
+}
