57085005fe0ab1400d9eb17c11c0c8f49a8f2971
Lukas Planz <lukas.planz@web.de>
2023-05-06 22:50:32 +0200

    Added async launchers

--- src/main/kotlin/com/okkero/skedule/Launchers.kt
+++ src/main/kotlin/com/okkero/skedule/Launchers.kt
@@ -4,6 +4,9 @@ import de.md5lukas.schedulers.AbstractScheduler
 import de.md5lukas.schedulers.Schedulers
 import java.util.concurrent.CompletableFuture
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Deferred
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.async
 import kotlinx.coroutines.future.future
 import kotlinx.coroutines.launch
 import org.bukkit.entity.Entity
@@ -12,21 +15,38 @@ import org.bukkit.plugin.Plugin
 fun Plugin.skedule(
     sync: SynchronizationContext = SynchronizationContext.ASYNC,
     block: suspend CoroutineScope.() -> Unit,
-) = Schedulers.global(this).skedule(sync, block)
+): Job = Schedulers.global(this).skedule(sync, block)
 
 fun Plugin.skedule(
     entity: Entity,
     sync: SynchronizationContext = SynchronizationContext.ASYNC,
     block: suspend CoroutineScope.() -> Unit,
-) = Schedulers.entity(this, entity).skedule(sync, block)
+): Job = Schedulers.entity(this, entity).skedule(sync, block)
 
 fun AbstractScheduler.skedule(
     sync: SynchronizationContext = SynchronizationContext.ASYNC,
     block: suspend CoroutineScope.() -> Unit,
-) {
-  CoroutineScope(BukkitDispatcher(this))
-      .launch(context = BukkitSchedulerSynchronizationContext(sync), block = block)
-}
+): Job =
+    CoroutineScope(BukkitDispatcher(this))
+        .launch(context = BukkitSchedulerSynchronizationContext(sync), block = block)
+
+fun <T> Plugin.async(
+    sync: SynchronizationContext = SynchronizationContext.ASYNC,
+    block: suspend CoroutineScope.() -> T,
+): Deferred<T> = Schedulers.global(this).async(sync, block)
+
+fun <T> Plugin.async(
+    entity: Entity,
+    sync: SynchronizationContext = SynchronizationContext.ASYNC,
+    block: suspend CoroutineScope.() -> T,
+): Deferred<T> = Schedulers.entity(this, entity).async(sync, block)
+
+fun <T> AbstractScheduler.async(
+    sync: SynchronizationContext = SynchronizationContext.ASYNC,
+    block: suspend CoroutineScope.() -> T,
+): Deferred<T> =
+    CoroutineScope(BukkitDispatcher(this))
+        .async(context = BukkitSchedulerSynchronizationContext(sync), block = block)
 
 fun <T> Plugin.future(
     sync: SynchronizationContext = SynchronizationContext.ASYNC,
@@ -42,7 +62,6 @@ fun <T> Plugin.future(
 fun <T> AbstractScheduler.future(
     sync: SynchronizationContext = SynchronizationContext.ASYNC,
     block: suspend CoroutineScope.() -> T,
-): CompletableFuture<T> {
-  return CoroutineScope(BukkitDispatcher(this))
-      .future(context = BukkitSchedulerSynchronizationContext(sync), block = block)
-}
+): CompletableFuture<T> =
+    CoroutineScope(BukkitDispatcher(this))
+        .future(context = BukkitSchedulerSynchronizationContext(sync), block = block)
